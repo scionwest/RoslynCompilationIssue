9b5ab8fb99f65f85a37d4b74c687ab0827c74f80
Johnathon Sullinger <Johnathon@sullinger.us>
2015-09-03 18:34:14 -0700

    Initial checkin of an application that can not run due to a potential Roslyn compiler bug.

---
+++ DotNet45App/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+using DotNet452Library;
+
+namespace DotNet45App
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            var libClass = new Class1();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
---
+++ DotNet45Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DotNet45Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
